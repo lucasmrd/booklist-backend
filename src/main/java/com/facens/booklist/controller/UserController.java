632ab4f4ef6d12c96b984a8624d2af77165f5c99
Lucas Miranda <lucasvnx6@gmail.com>
2024-11-05 16:52:19 -0300

    Removendo anotação

--- src/main/java/com/facens/booklist/controller/UserController.java
+++ src/main/java/com/facens/booklist/controller/UserController.java
@@ -6,7 +6,6 @@ import com.facens.booklist.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
