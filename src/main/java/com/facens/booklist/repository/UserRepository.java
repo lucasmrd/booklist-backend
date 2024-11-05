90a89e15b0f15778ba4cb2b0dc8209e7eb01c8be
Lucas Miranda <lucasvnx6@gmail.com>
2024-11-05 14:30:31 -0300

    CouchbaseConfig e application.properties

--- src/main/java/com/facens/booklist/repository/UserRepository.java
+++ src/main/java/com/facens/booklist/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.facens.booklist.repository;
 
 import com.facens.booklist.entity.User;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CouchbaseRepository<User, String> {
 }
